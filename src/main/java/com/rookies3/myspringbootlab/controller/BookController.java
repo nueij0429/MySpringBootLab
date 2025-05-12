72600a15082f2a06d2009961d3da365a8efea3d5
nueij0429 <ssjjjieun0429@gmail.com>
2025-05-12 19:41:41 +0900

    2-4 controller 양식 수정

--- src/main/java/com/rookies3/myspringbootlab/controller/BookController.java
+++ src/main/java/com/rookies3/myspringbootlab/controller/BookController.java
@@ -16,35 +16,35 @@ import java.util.Optional;
 public class BookController {
     private final BookService bookService;
 
-    // 도서 등록
-    @PostMapping
-    public ResponseEntity<BookDTO.BookResponse> createBook(
-            @Valid @RequestBody BookDTO.BookCreateRequest request) {
-        BookDTO.BookResponse response = bookService.createBook(request);
-        return ResponseEntity.ok(response);
-    }
-
     // 전체 도서 목록 조회
     @GetMapping
-    public ResponseEntity<List<BookDTO.BookResponse>> getAllBooks() {
+    public ResponseEntity<List<BookDTO.Response>> getAllBooks() {
         return ResponseEntity.ok(bookService.getAllBooks());
+    }
+
+    // 도서 등록
+    @PostMapping
+    public ResponseEntity<BookDTO.Response> createBook(
+            @Valid @RequestBody BookDTO.Request request) {
+        BookDTO.Response response = bookService.createBook(request);
+        return ResponseEntity.ok(response);
     }
 
     // ID로 도서 조회
     @GetMapping("/{id}")
-    public ResponseEntity<BookDTO.BookResponse> getBookById(@PathVariable Long id) {
+    public ResponseEntity<BookDTO.Response> getBookById(@PathVariable Long id) {
         return ResponseEntity.ok(bookService.getBookById(id));
     }
 
     // ISBN으로 도서 조회
     @GetMapping("/isbn/{isbn}")
-    public ResponseEntity<BookDTO.BookResponse> getBookByIsbn(@PathVariable String isbn) {
+    public ResponseEntity<BookDTO.Response> getBookByIsbn(@PathVariable String isbn) {
         return ResponseEntity.ok(bookService.getBookByIsbn(isbn));
     }
 
     // 도서 정보 수정
     @PatchMapping("/{id}")
-    public ResponseEntity<BookDTO.BookResponse> updateBook(@PathVariable Long id,
+    public ResponseEntity<BookDTO.Response> updateBook(@PathVariable Long id,
                                                            @Valid @RequestBody BookDTO.BookUpdateRequest request) {
         return ResponseEntity.ok(bookService.updateBook(id, request));
     }

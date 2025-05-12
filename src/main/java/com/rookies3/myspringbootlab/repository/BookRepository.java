de4cf93923966023ce20ff141eb667382dcfb03d
nueij0429 <ssjjjieun0429@gmail.com>
2025-05-12 19:08:20 +0900

    2-4 repository 생성

--- src/main/java/com/rookies3/myspringbootlab/repository/BookRepository.java
+++ src/main/java/com/rookies3/myspringbootlab/repository/BookRepository.java
@@ -2,11 +2,24 @@ package com.rookies3.myspringbootlab.repository;
 
 import com.rookies3.myspringbootlab.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book,Long> {
     Optional<Book> findByIsbn(String isbn);
-    List<Book> findByAuthor(String author);
+
+    List<Book> findByAuthorContainingIgnoreCase(String author);
+
+    List<Book> findByTitleContainingIgnoreCase(String title);
+
+    @Query("SELECT b FROM Book b JOIN FETCH b.bookDetail WHERE b.id = :id")
+    Optional<Book> findByIdWithBookDetail(@Param("id") Long id);
+
+    @Query("SELECT b FROM Book b JOIN FETCH b.bookDetail WHERE b.isbn = :isbn")
+    Optional<Book> findByIsbnWithBookDetail(@Param("isbn") String isbn);
+
+    boolean existsByIsbn(String isbn);
 }
